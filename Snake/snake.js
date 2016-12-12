27054a3adf43f105aec9b97c103d59ebd16bf6ae
Dave Machado <dave@davemachado.com>
2016-12-11 19:09:30 -0500

    Add Music and Game Over state

--- Snake/snake.js
+++ Snake/snake.js
@@ -26,11 +26,12 @@ function Snake() {
             var pos = this.tail[i];
             var d = dist(this.x, this.y, pos.x, pos.y);
             if (d < 1) {
-                console.log("starting over");
                 this.total = 0;
                 this.tail = [];
+                return true;
             }
         }
+        return false;
     };
 
     this.update = function() {
